5ef41bdcc28cccfbbf73b73ee988d18979e61a63
NomikLee <nomiklee@icloud.com>
2024-05-09 09:18:46 +0800

    Initial Commit

---
+++ NomikCrypto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NomikCrypto
+//
+//  Created by Pinocchio on 2024/5/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
